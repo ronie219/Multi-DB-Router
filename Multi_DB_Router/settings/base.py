d06e60a0ea552fe38a5b057eb15d45ea467bceff
ronie219 <abhishek.biswas219@gmail.com>
2020-10-11 23:00:52 +0530

    Update base.py

--- Multi_DB_Router/settings/base.py
+++ Multi_DB_Router/settings/base.py
@@ -4,7 +4,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
-print(TEMPLATE_DIR)
+#print(TEMPLATE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
